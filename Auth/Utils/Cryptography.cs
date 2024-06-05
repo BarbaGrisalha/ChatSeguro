3fa9093aa561cc5cd709465abc37511b9e3e7b8b
Lucas Siqueira <lucassiqueira0763@hotmail.com>
2024-06-05 16:34:06 +0100

    ajuste
    apaguei coisas desnecessarias e renomeei outras

---
+++ Auth/Utils/Cryptography.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChatSeguro.Auth.Utils
+{
+    public static class Cryptography
+    {
+        // Método para gerar o hash da senha com salt
+        public static string GenerateHash(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password + salt); // Concatena a senha com o salt
+                var hash = sha256.ComputeHash(bytes); // Gera o hash
+                return BitConverter.ToString(hash).Replace("-", ""); // Converte o hash para string
+            }
+        }
+
+        // Método para gerar um salt
+        public static string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[32]; // Cria um array de bytes para o salt
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes); // Gera bytes aleatórios para o salt
+            }
+            return BitConverter.ToString(saltBytes).Replace("-", ""); // Converte o salt para string
+        }
+    }
+}
---
+++ Auth/register.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Auth.Controllers;
+using Auth;
+using ChatSeguro.Auth;
+
+namespace Auth
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            string username = textUsername.Text;
+            string password = textPassword.Text;
+            string password2 = textConPassword.Text;
+
+            List<string> errors = PasswordValidator.ValidatePassword(password);
+            labelWarning.Text = "";
+
+            if (errors.Any())
+            {
+                foreach (string erro in errors)
+                {
+                    labelWarning.Text += $"\n{erro}";
+                }
+                labelWarning.Visible = true;
+            }
+            else if (PasswordValidator.ComparePasswords(password, password2))
+            {
+                labelConPassword.Text = "";
+                labelConPassword.Text = "Passwords do not match, Please Re-enter";
+                textPassword.Text = "";
+                textConPassword.Text = "";
+                textPassword.Focus();
+                labelConPassword.Visible = true;
+            }
+            else
+            {
+                // Chama o método Register no AuthController para registrar o usuário
+                AuthController.Register(username, password);
+                MessageBox.Show("Usuário criado com sucesso");
+
+                textUsername.Text = "";
+                textPassword.Text = "";
+                textConPassword.Text = "";
+                textUsername.Focus();
+            }
+        }
+
+        private void textConPassword_Enter(object sender, EventArgs e)
+        {
+            string username = textUsername.Text;
+            string password = textPassword.Text;
+
+            List<string> errors = PasswordValidator.ValidatePassword(password);
+
+            if (errors.Any())
+            {
+                labelWarning.Text = "";
+                foreach (string erro in errors)
+                {
+                    labelWarning.Text += $"\n{erro}";
+                }
+                labelWarning.Visible = true;
+            }
+        }
+
+        private void labelLogin_Click(object sender, EventArgs e)
+        {
+            // Abre a janela de login
+            new LoginForm().Show();
+            this.Hide();
+        }
+
+        private void checkBoxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxShowPas.Checked)
+            {
+                textPassword.PasswordChar = '\0';
+                textConPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                textPassword.PasswordChar = '●';
+                textConPassword.PasswordChar = '●';
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Limpa os campos e foca no campo do username
+            textUsername.Text = "";
+            textPassword.Text = "";
+            textConPassword.Text = "";
+            textUsername.Focus();
+        }
+
+        
+    }
+}
